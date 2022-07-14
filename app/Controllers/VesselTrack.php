128d7fb8b0960ab182ebdc362e8611f95c54adf1
magnawealth <tobiwealth.me@gmail.com>
2022-07-14 03:35:00 +0100

    Controller added

---
+++ app/Controllers/VesselTrack.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\VesselTrackModel;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\ResponseTrait;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Repository\VesselTrackRepository;
+use Stringable;
+
+class VesselTrack extends ResourceController
+{
+    use ResponseTrait;
+
+    protected $repository;
+
+    public function __construct() {
+        $this->repository = new VesselTrackRepository();
+    }
+
+    public function index(): ResponseInterface
+    {
+        $data = $this->repository->getAll();
+        return $this->respond($data);
+    }
+    
+    public function getByMMSI(): ResponseInterface
+    {
+        $mmsi = $this->request->getVar("mmsi") ? explode(',', $this->request->getVar("mmsi")) : 0;
+        $data = (count($mmsi) >= 1) ? $this->repository->getByMMSI($mmsi) : "Record Not Found";
+        return $this->respond($data);
+    }
+
+    public function getByPosition(): ResponseInterface
+    {
+        $lat = $this->request->getVar("lat") ? $this->request->getVar("lat") : 0; 
+        $lon = $this->request->getVar("lon") ? $this->request->getVar("lon") : 0;
+        $data = (!empty($lat) && !empty($lon)) 
+                            ? $this->repository->getByPosition($lat, $lon) 
+                            : "Record Not Found";
+        return $this->respond($data);
+    }
+
+    public function getByTimeInterval(): ResponseInterface
+    {
+        $startDate = $this->request->getVar("startDate") ? $this->request->getVar("startDate") : 0; 
+        $endDate = $this->request->getVar("endDate") ? $this->request->getVar("endDate") : 0;
+        $data = (!empty($startDate) && !empty($endDate)) 
+                            ? $this->repository->getByTimeInterval($startDate, $endDate) 
+                            : "Record Not Found";
+        return $this->respond($data);
+
+    }
+
+    public function save(): ResponseInterface
+    {
+        $model = new VesselTrackModel();
+        // $data['vesseltrack'] = $model->findAll();
+        $data = $model->findAll();
+        return $this->respond($data);
+    }
+}
