97978abf6d1b637677274d17b7295a643059a7cc
magnawealth <tobiwealth.me@gmail.com>
2022-07-15 17:19:07 +0100

    service updated

--- app/Services/VesselTrackService.php
+++ app/Services/VesselTrackService.php
@@ -4,5 +4,29 @@ namespace App\Services;
 
 class VesselTrackService
 {
+    public $contentTypeArray = [
+        'application/json',
+        'application/ld+json',
+        'application/hal+json',
+        'application/vnd.api+json',
+        'application/xml',
+        'text/csv'
+    ];
+    
+    public function isValidContentType($contentType)
+    {
+        if ($contentType == "is found in contentTypeArray") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function saveFileUploadToPath($file, $uploadPath)
+    {
+        # code...
+    }
+
     
 }
