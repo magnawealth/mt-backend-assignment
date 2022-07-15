58fd67050cb9c67f327d41702904fe5e68fa7ed8
magnawealth <tobiwealth.me@gmail.com>
2022-07-15 22:18:03 +0100

    Factory added

---
+++ app/Factory/VesselTrackFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Factory;
+
+use App\Controllers\BaseController;
+
+class VesselTrackFactory extends BaseController
+{
+    public function createArrayFromJson($filePath)
+    {
+        $contents = file_get_contents($filePath);
+        return ((string) $contents !== '') ? json_decode($contents, true) : null ;
+    }
+
+    public function convertXmlToArray($filePath)
+    {
+        $contents = file_get_contents($filePath);
+        return ((string) $contents !== '') ? json_decode($contents, true) : null ;
+    }
+
+    public function convertCsvToArray($filePath)
+    {
+        $contents = file_get_contents($filePath);
+        return ((string) $contents !== '') ? json_decode($contents, true) : null ;
+    }
+}
